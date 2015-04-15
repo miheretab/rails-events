913bf260ea4f6c1691201c1c9522d061e6f28710
Miheretab <mihrtab@gmail.com>
2015-04-15 18:33:49 +0300

    Debugging allDay

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,6 +11,6 @@ class EventsController < ApplicationController
 	
 	private
 	  def event_params
-		params.require(:Event).permit(:title, :start, :end)
+		params.require(:Event).permit(:title, :start, :end, :allDay)
 	  end	
 end
