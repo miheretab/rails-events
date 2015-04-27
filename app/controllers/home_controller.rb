5dfd44c91e76eae482d6ba5f870dc939af110662
Miheretab <mihrtab@gmail.com>
2015-04-27 18:26:39 +0300

    Fixing IP feature

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
 	@events = []
-	Event.where(:ip_address => '127.0.0.1').each do |event|
+	Event.where(:ip_address => request.remote_ip).each do |event|
 		@events << {'id' => event.id, 'title' => event.title, 'start' => event.start_time, 'end' => event.end_time, 'allDay' => event.all_day}
 	end
   end
